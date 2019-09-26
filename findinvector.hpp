37727f2e22aea555d21e68f762efcef4b87cde59
armandj <armandj@netstar.co.za>
2019-09-26 14:54:19 +0200

    Header file added

---
+++ findinvector.hpp
@@ -0,0 +1,10 @@
+#ifndef FINDINVECTOR_H_INCLUDED
+#define FINDINVECTOR_H_INCLUDED
+
+#include <utility>
+#include <vector>
+
+template < typename T>
+std::pair<bool, int > findInVector(const std::vector<T>  & vecOfElements, const T  & element);
+
+#endif // FINDINVECTOR_H_INCLUDED
